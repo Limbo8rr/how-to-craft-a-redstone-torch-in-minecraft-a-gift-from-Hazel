bd7a15b0b66ee995d971b62fbd54d0e559482333
Limbo8rr <77548425+Limbo8rr@users.noreply.github.com>
2021-03-14 18:42:13 -0700

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,22 @@
 function startFest () {
-    if (Math.percentChance(20)) {
+    if (true) {
         game.showLongText("in minecraft to craft a redstone torch, you will need to go get some wood, then you craft all that wood into oak planks, with the oak planks put the op(oak planks) in all four corners, then you will get a crafting table, then place the ct(crafting table) down and right click on it. then use your remaining op and craft a pickaxe you can see all the crafts if you go in the left side you can go start diging a cave, once you've got at least 3 stone you can upgrade your wooden pick to a stone pick. and if you end up finding 3 iron befor you find redstone(redstone is a red ore that looks like stone but has pixels of red inside it.) you can upgrade your stone pick to an iron pick. once you've got redstone go back to your ct(crafting table). once you've gotten to your crafting table get one stick and put it in any slot as long as their's a slot above it and in that slot above it guess what? yep thats why you got the redstone you probably gussed it, place the redstone on top of the stick and you know what you got a redstone torch.", DialogLayout.Full)
         tiles.setTilemap(tilemap`level1`)
+        pause(5000)
+        game.splash("time for the pickaxe craft. no.1")
+        tiles.setTilemap(tilemap`level4`)
+        pause(5000)
+        game.splash("time for the pickaxe craft. no.2")
+        tiles.setTilemap(tilemap`level6`)
     }
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     startFest()
 })
 controller.A.onEvent(ControllerButtonEvent.Released, function () {
-    startFest()
+	
 })
 controller.A.onEvent(ControllerButtonEvent.Repeated, function () {
-    startFest()
+	
 })
 startFest()
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -12,17 +12,95 @@ namespace myTiles {
     export const tile4 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "level3":
+            case "level3":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "level5":
+            case "level5":return tiles.createTilemap(hex`0a0005000101010101000000000001060606010000000000010005000100030004000100020001000000000001010101010000000000`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile3,myTiles.tile5,myTiles.tile4,myTiles.tile2,myTiles.tile10], TileScale.Sixteen);
             case "level1":
-            case "level1":return tiles.createTilemap(hex`0a0005000101010101000000000001000000010000000000010002000100050004000100030001000000000001010101010000000000`, img`
+            case "level1":return tiles.createTilemap(hex`0a0005000101010101000000000001000000010000000000010005000100030004000100020001000000000001010101010000000000`, img`
 . . . . . . . . . . 
 . . . . . . . . . . 
 . . . . . . . . . . 
 . . . . . . . . . . 
 . . . . . . . . . . 
-`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5], TileScale.Sixteen);
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile3,myTiles.tile5,myTiles.tile4,myTiles.tile2,myTiles.tile7,myTiles.tile6,myTiles.tile8,myTiles.tile9,myTiles.tile10], TileScale.Sixteen);
+            case "level6":
+            case "level6":return tiles.createTilemap(hex`0a0005000101010101000000000001040404010000000000010002000100030005000100020001000000000001010101010000000000`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile3,myTiles.tile5,myTiles.tile11,myTiles.tile12,myTiles.tile13], TileScale.Sixteen);
+            case "level4":
+            case "level4":return tiles.createTilemap(hex`0a0005000101010101000000000001040404010000000000010002000100030005000100020001000000000001010101010000000000`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile3,myTiles.tile5,myTiles.tile10,myTiles.tile9], TileScale.Sixteen);
         }
         return null;
     })
@@ -40,6 +118,22 @@ namespace myTiles {
             case "tile4":return tile4;
             case "myTile3":
             case "tile5":return tile5;
+            case "myTile5":
+            case "tile7":return tile7;
+            case "myTile4":
+            case "tile6":return tile6;
+            case "myTile6":
+            case "tile8":return tile8;
+            case "myTile7":
+            case "tile9":return tile9;
+            case "myTile8":
+            case "tile10":return tile10;
+            case "myTile9":
+            case "tile11":return tile11;
+            case "myTile10":
+            case "tile12":return tile12;
+            case "myTile11":
+            case "tile13":return tile13;
         }
         return null;
     })
